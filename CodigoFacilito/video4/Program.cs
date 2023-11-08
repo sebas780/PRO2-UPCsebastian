c5abad99ebc223a3991a06f13289e11a263d7138
sebas780 <149842430+sebas780@users.noreply.github.com>
2023-11-07 21:32:09 -0400

    video4

---
+++ CodigoFacilito/video4/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace presentacion4
+{
+    classe program
+   {
+    static void Main(string[] args)
+   {  
+   }
+   }
+}
+         int i= 10;
+        decimal x = 12.2m;  
+        bool bandera = false;
+        string cadena = string.Empty;
+        DateTime fecha = DateTime.MinValue;
+        i = (int)x;
+        Console.WriteLine("El valor de i es: (0)", i);
+      Console.WriteLine("El valor de x es: (0:C)", x);
+    Console.WriteLine("El valor de bandera es: bandera.ToString());
+    Console.WriteLine("El valor de cadena: cadena);
+    Console.WriteLine("El valor de fecha: fecha. ToShortDateString());
